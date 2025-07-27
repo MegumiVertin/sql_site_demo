13b0efb83a9ae37a734e36ae3dddf77870568f30
MegumiVertin <161516981+MegumiVertin@users.noreply.github.com>
2025-07-26 21:17:51 -0400

    Update wsgi.py

--- sql_site/wsgi.py
+++ sql_site/wsgi.py
@@ -2,15 +2,13 @@
 WSGI config for sql_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+For details, see https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sql_site.settings')
+# Ensure the correct settings module is loaded
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sql_site.settings")
 
 application = get_wsgi_application()
